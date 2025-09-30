851a415de0f4d198ea65ab9a50c1a681f29d91d7
Mohammed Amin <mdamin67541@gmail.com>
2025-09-30 18:03:36 +0530

    Updated deployment of Meteor Madness

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import nasaRoutes from './routes/nasa.js';
 import "dotenv/config";
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', nasaRoutes);
 
